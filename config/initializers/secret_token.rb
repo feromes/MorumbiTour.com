45026af63e58b8e5363311248cff00d33442f3e8
Fernando <feromes@gmail.com>
2012-01-03 11:08:05 -0200

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MorumbitourCom::Application.config.secret_token = '6eaff0a3f70f9718204f6b118a6197450fe1634aaad05e61b5a8e533709545485c3f236a1f5e900185a94650b980014bd5d77d9a214a0d69dab0ac29f10c3727'
