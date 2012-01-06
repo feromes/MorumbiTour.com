059c8fdeac9cfd32102bc4a5a776b29b82e956cd
Fernando <feromes@gmail.com>
2012-01-06 14:04:41 -0200

    Usuarios não staff restritos

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "email#{n}@example.com" }
     password "foobar"
+    staff true
     # email { "#{username}@example.com" }
     
     factory :admin do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 
 describe User do
   it "Ao criar um usuário novo deve ser criado como não sendo Staff" do
-    pending
+    user = User.create! email: "user@exemplo.com",
+                        password: "foobar"
+    user.staff.should_not be_true
   end
 end
